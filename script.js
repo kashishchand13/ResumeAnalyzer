a92451af0ef2631a50dc02c6cce641c71d63f11c
jeevanchandrbhatt-byte <jeevanchandrbhatt@gmail.com>
2026-04-13 18:16:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function analyze() {
+
+    let resume = document.getElementById("resume").value;
+    let job = document.getElementById("job").value;
+
+    fetch("http://localhost:8081/api/analyze", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            resumeText: resume,
+            jobDescription: job
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("result").innerHTML =
+                "Match Score: " + data.score +
+                "<br>Missing Skills: " + data.missingSkills;
+        });
+
+}
